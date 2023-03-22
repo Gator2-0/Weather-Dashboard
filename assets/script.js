34655294cfab62e44857d91061b0f44712f1839b
Jeremy <jejedech.63@gmail.com>
2023-03-22 21:54:49 +1100

    adding render blank function

--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,31 @@
 const apiKey = 'd90d1ffad25d55d6207980f2e62347a6'
-let city = 'Sydney'
-const longlatUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid="+ apiKey;
-let coordonates = [];
+let city ='';
+
+let searchButton = $('#search-button');
+let inputBar = $('#input-bar');
 
 
-function getCurrentWeather(requestUrl){
-  console.log('inside getCurrentWeather function');
-  fetch(requestUrl)
-    .then(function (response){
-      console.log(response);
-      if(response.status == 200){
-        console.log('response status is '+ response.status)
-      }
-      return response.json()
-    })
-    .then(function (data){ 
-      console.log(data[0])
-      coordonates.push(data[0].lat);
-      coordonates.push(data[0].lon)
-      console.log('lat is: '+ data[0].lat);
-      console.log('long is: '+data[0].lon);
-      console.log(coordonates);
-      return coordonates
-    })
-    .then(function (coordonates){
-      console.log('in getweather function')
-      let currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat="+coordonates[0]+"&lon="+coordonates[1]+"&units=metric&&appid=" + apiKey;
-      fetch(currentWeatherUrl)
-        .then(function (response){
-          console.log(response);
-          if(response.status == 200){
-            console.log('response status is '+ response.status)
-          }
-
-          return response.json()
-        })
-        .then(Current(data,''))
-    })
-    .then(function(){
-      let forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+coordonates[0]+'&lon='+coordonates[1]+'&appid=' + apiKey;
-      fetch(forecastUrl)
-      .then(function(response){
-        if(response.status !== 200){
-          console.log('fetch forecast status= '+ response.status)
-        }
-        return response.json();
-      })
-      .then(function (data){
-        console.log(data);
-      })
-    })   
-}
 
 function displayCurrentWeatherData(object, htmlLocation){
   console.log(object)
-          let city = $("<p>").text(object.name); 
-          console.log(city.text())
-          let temp = $("<p>").text(object.main.temp);
-          console.log(temp.text());
-          let wind= $("<p>").text(object.wind.speed);
-          console.log(wind.text())
-          let hum = $("<p>").text(object.main.humidity);
-          console.log(hum.text())
+          let city = $("<strong>").text(object.name+' '+ dayjs().format('(d/m/YYYY)'));
+          let icon = $("<img>").attr({'src':'http://openweathermap.org/img/w/'+object.weather[0].icon+'.png'}); 
+          let temp = $("<p>").text('Temp: '+ object.main.temp +'°C');     
+          let wind= $("<p>").text('Wind: '+ object.wind.speed + 'KM/h');      
+          let hum = $("<p>").text('Humidity: '+ object.main.humidity+ '%');
 
-          $(htmlLocation).append(city,temp, wind, hum);
+          $(htmlLocation).append(city,icon,temp,wind,hum);
 }
 
 function displayForecastWeatherData(object, htmlLocation){
-  let card = $("<div>").addClass('.card');
+  let card = $("<div>").addClass('card');
 
-  let date = $("<p>").text(object.dt_txt.split(' ')[0]);
-  let icon = $("<img>").attr('src',"http://openweathermap.org/img/w/"+object.weather[0].icon+".png");
+  let date = $("<strong>").text(object.dt_txt.split(' ')[0]);
+  let icon = $("<img>").attr({'src':'http://openweathermap.org/img/w/'+object.weather[0].icon+'.png'});
   let temp = $("<p>").text('Temp: '+ object.main.temp +'°C') ;
   let wind = $("<p>").text('Wind: '+ object.wind.speed + 'KM/h');
   let hum = $("<p>").text('Humidity: '+ object.main.humidity+ '%');
+
   card.append(date,icon,temp,wind,hum);
   $(htmlLocation).append(card)
 }
@@ -96,7 +48,7 @@ async function getForecast(){
   for (let index = 0; index < days.length; index++) {
     let selectedDay = dataForecast[days[index]];
     console.log(selectedDay);
-    displayForecastWeatherData(selectedDay, '.card-divider');
+    displayForecastWeatherData(selectedDay, '.card-section');
     
   }
   
@@ -110,12 +62,49 @@ async function getCurrentWeather(){
   displayCurrentWeatherData(currentWeatherObject, '.current-weather');
 }
 
+function renderBlank(){
+  $(".card-section").children().remove();
+  $(".current-weather").children().remove();
+  $(".previous-search").children().remove();
+}
+
+function displayPreviousSearch(){
+  let previoussearch = JSON.parse(localStorage.getItem('citySearched'));
+  if(!previoussearch){
+    return
+  }else{
+    for (let index = 0; index < previoussearch.length; index++) {
+      let search = previoussearch[index];
+      let li =$("li").text(search).addClass(list)
+      $(".previous-search").append(li);
+    }
+  }
+}
 
 
-getForecast();
-getCurrentWeather();
-//getCurrentWeather(longlatUrl);
-//console.log(coordonates)
+
+
+
+searchButton.on('click',function(){
+  city = inputBar.val();
+  console.log('the city chosen is: '+ city)
+  renderBlank();
+  getForecast();
+  getCurrentWeather();
+
+  let previoussearch = JSON.parse(localStorage.getItem('citySearched'));
+  console.log('Local storage is : '+ previoussearch)
+  if(previoussearch == null){
+    previoussearch += city;
+    localStorage.setItem('citySearched',JSON.stringify(city))
+  }else{
+    previoussearch.push(city)
+  }
+
+   
+   
+});
+
 
 
 
